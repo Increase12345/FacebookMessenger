c7307da412a575fee9298d6977268650956084a2
Nick <pretki123@ukr.net>
2023-06-30 13:33:18 -0600

    getting started

---
+++ FacebookMessenger/App/FacebookMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookMessengerApp.swift
+//  FacebookMessenger
+//
+//  Created by Nick Pavlov on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FacebookMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FacebookMessenger/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  FacebookMessenger
+//
+//  Created by Nick Pavlov on 6/30/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
