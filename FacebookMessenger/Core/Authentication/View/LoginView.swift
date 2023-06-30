59d9f8b2baa89f1b39c2dc215c2dcf37b420f324
Nick <pretki123@ukr.net>
2023-06-30 14:11:21 -0600

    started working on login View

--- FacebookMessenger/Core/Authentication/View/LoginView.swift
+++ FacebookMessenger/Core/Authentication/View/LoginView.swift
@@ -8,8 +8,88 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                // logo image
+                Image("messengerLogo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .padding()
+                
+                // text fields
+                VStack {
+                    TextField("Enter your email", text: $email)
+                    SecureField("Enter your password", text: $password)
+                }
+                
+                // forgot password
+                Button {
+                    
+                } label: {
+                    Text("Forgot password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                        .padding(.trailing, 28)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                // login button
+                Button {
+                    
+                } label: {
+                    Text("Login")
+                    
+                }
+                
+                
+                // facebook login
+                HStack {
+                    // Divider
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                }
+                .foregroundColor(.secondary)
+                
+                // Continue with facebook Button
+                HStack {
+                    Image("facebookLogo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20)
+                    Text("Continue with Facebook")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.blue)
+                }
+                .padding(.top, 8)
+                
+                Spacer()
+                
+                
+                // sign up link
+                NavigationLink {
+                    
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+            }
+        }
     }
 }
 
