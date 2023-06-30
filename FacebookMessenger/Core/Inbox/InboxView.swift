bdab9bb5776ce2838d7b09a2deb8da6d1c6f0e08
Nick <pretki123@ukr.net>
2023-06-30 14:29:57 -0600

    created Inbox view

---
+++ FacebookMessenger/Core/Inbox/InboxView.swift
@@ -0,0 +1,45 @@
+//
+//  InboxView.swift
+//  FacebookMessenger
+//
+//  Created by Nick Pavlov on 6/30/23.
+//
+
+import SwiftUI
+
+struct InboxView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    HStack {
+                        Image(systemName: "person.circle.fill")
+                        
+                        Text("Chats")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "square.and.pencil.circle.fill")
+                            .resizable()
+                            .frame(width: 32, height: 32)
+                            .foregroundStyle(.black, Color(.systemGray5))
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct InboxView_Previews: PreviewProvider {
+    static var previews: some View {
+        InboxView()
+    }
+}
