3a85b46fd4f0958b7ea6c1be5d9a60213b224ca7
Nick <pretki123@ukr.net>
2023-07-05 13:20:02 -0600

    working on message view

---
+++ FacebookMessenger/Core/Chat/ChatView.swift
@@ -0,0 +1,59 @@
+//
+//  ChatView.swift
+//  FacebookMessenger
+//
+//  Created by Nick Pavlov on 7/5/23.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    @State private var messageText = ""
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                // header
+                VStack {
+                    CircularProfileImageView(user: User.MOCK_USER, size: .xLarge)
+                    
+                    VStack(spacing: 4) {
+                        Text("Nick Pavlov")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                        Text("Messenger")
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                    }
+                }
+                
+                // messages
+                
+            }
+            // message input view
+            ZStack(alignment: .trailing) {
+                TextField("Message...", text: $messageText, axis: .vertical)
+                    .padding(12)
+                    .padding(.trailing, 48)
+                    .background(Color(.systemGroupedBackground))
+                    .clipShape(Capsule())
+                    .font(.subheadline)
+                
+                Button {
+                    
+                } label: {
+                    Text("Send")
+                        .fontWeight(.semibold)
+                }
+                .padding(.horizontal)
+            }
+            .padding()
+        }
+    }
+}
+
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatView()
+    }
+}
