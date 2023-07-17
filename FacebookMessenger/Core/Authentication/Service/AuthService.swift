0c6189f8daa7599738e7f17dd4bb65edc37e9726
Nick <pretki123@ukr.net>
2023-07-16 21:11:44 -0600

    done with signUp, login and sign out functionality and logic

--- FacebookMessenger/Core/Authentication/Service/AuthService.swift
+++ FacebookMessenger/Core/Authentication/Service/AuthService.swift
@@ -11,20 +11,36 @@ import Firebase
 class AuthService {
     @Published var userSession: FirebaseAuth.User?
     
+    static let shared = AuthService()
+    
     init() {
         self.userSession = Auth.auth().currentUser
     }
     
     func login(with email: String, password: String) async throws {
-        //try await Auth.auth().signIn(withEmail: email, password: password)
+        do {
+            let result = try await Auth.auth().signIn(withEmail: email, password: password)
+            self.userSession = result.user
+        } catch {
+            print("DEBUG: Failed to signIn \(error.localizedDescription)")
+        }
     }
     
     func createUser(with email: String, password: String, fullName: String) async throws {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
-            print("DEBUG: created user \(result.user.uid))")
+            self.userSession = result.user
         } catch {
             print("DEBUG: Faidel to create user \(error.localizedDescription)")
+        }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut() // signOut on backend
+            self.userSession = nil // updates routing logic
+        } catch {
+            print("DEBUG: Failed to signOut \(error.localizedDescription)")
         }
     }
 }
