4a8144cf1df81b2361c3f9e8278e89c4f13838b6
Nick <pretki123@ukr.net>
2023-07-28 13:20:29 -0600

    send messages functionality done

--- FacebookMessenger/Core/Chat/View/ChatView.swift
+++ FacebookMessenger/Core/Chat/View/ChatView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct ChatView: View {
-    @State private var messageText = ""
+    @StateObject var vm: ChatViewModel
     let user: User
+    
+    init(user: User) {
+        self.user = user
+        self._vm = StateObject(wrappedValue: ChatViewModel(user: user))
+    }
     
     var body: some View {
         VStack {
@@ -36,7 +41,7 @@ struct ChatView: View {
             }
             // message input view
             ZStack(alignment: .trailing) {
-                TextField("Message...", text: $messageText, axis: .vertical)
+                TextField("Message...", text: $vm.messageText, axis: .vertical)
                     .padding(12)
                     .padding(.trailing, 48)
                     .background(Color(.systemGroupedBackground))
@@ -44,7 +49,8 @@ struct ChatView: View {
                     .font(.subheadline)
                 
                 Button {
-                    
+                    vm.sendMessage()
+                    vm.messageText = ""
                 } label: {
                     Text("Send")
                         .fontWeight(.semibold)
---
+++ FacebookMessenger/Model/Message.swift
@@ -0,0 +1,32 @@
+//
+//  Message.swift
+//  FacebookMessenger
+//
+//  Created by Nick Pavlov on 7/28/23.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct Message: Codable, Identifiable, Hashable {
+    @DocumentID var messageId: String?
+    let fromId: String
+    let toId: String
+    let messageText: String
+    let timestamp: Timestamp
+    
+    var user: User?
+    
+    var id: String {
+        messageId ?? NSUUID().uuidString
+    }
+    
+    var chatPartnerId: String {
+        fromId == Auth.auth().currentUser?.uid ? toId: fromId
+    }
+    
+    var isFromCurrentUser: Bool {
+        fromId == Auth.auth().currentUser?.uid
+    }
+}
