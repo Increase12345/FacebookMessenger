a61b34f027d54010af088c27c99a6efd20ae3cb6
Nick <pretki123@ukr.net>
2023-07-31 14:24:23 -0600

    implemented chat messages listener

---
+++ FacebookMessenger/Core/Chat/ViewModel/ChatViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  ChatViewModel.swift
+//  FacebookMessenger
+//
+//  Created by Nick Pavlov on 7/28/23.
+//
+
+import Foundation
+
+class ChatViewModel: ObservableObject {
+    @Published var messageText = ""
+    @Published var messages = [Message]()
+    
+    let user: User
+    
+    init(user: User) {
+        self.user = user
+        observeMessages()
+    }
+    
+    func observeMessages() {
+        MessageService.observeMessages(chatPartner: user) { messages in
+            self.messages.append(contentsOf: messages)
+        }
+    }
+    
+    func sendMessage() {
+        MessageService.sendMessage(messageText, toUser: user)
+    }
+}
